439cc96af7bc25c6c34017a63700546095229cdd
Julia Lavagnini Maia <julia.lavagninimaia@gmail.com>
2023-09-14 22:18:37 +0100

    Add game variables

---
+++ main.js
@@ -0,0 +1,4 @@
+const startBtn = document.querySelector("#startBtn");
+const countCenter = document.querySelector("#count");
+const colorPart = document.querySelectorAll(".color-part");
+const higherScore = document.querySelector("#higherScore");
