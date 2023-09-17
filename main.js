6af0252d75cfa7e2d6b3e7f5250cf12a913c1250
Julia Lavagnini Maia <julia.lavagninimaia@gmail.com>
2023-09-17 23:12:47 +0100

    Organise code and build the gameplay

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const higherScore = document.querySelector("#higherScore");
 const wrapper = document.querySelector(".wrapper");
 const container = document.querySelector(".container");
 
-
 // Mapping colors objects
 const colors = {
   color1: {
@@ -28,38 +27,39 @@ const colors = {
 
 let randomColors = [];
 let pathGenerator = false;
-let count, clickCount = 0;
+let count,
+  clickCount = 0;
 
 //Start the game
 const startGame = () => {
-    count = 0;
-    clickCount = 0;
-    randomColors = [];
-    pathGenerator = false;
-    wrapper.classList.remove("main");
-    container.classList.add("main");
-    pathway();
+  count = 0;
+  clickCount = 0;
+  randomColors = [];
+  pathGenerator = false;
+  wrapper.classList.remove("main");
+  container.classList.add("main");
+  pathway();
 };
 
 //Function to decide and play the sequence
 const pathway = () => {
-    randomColors.push(getRandomValue(colors));
-    count = randomColors.length;
-    pathGenerator = true;
-    pathwayPlay(count);
+  randomColors.push(getRandomValue(colors));
+  count = randomColors.length;
+  pathGenerator = true;
+  pathwayPlay(count);
 };
 
 const pathwayPlay = async (count) => {
-    countCenter.innerText = count;
-    for (let item of randomColors){
-        let currentColor = document.querySelector(`.${item}`);
-        await delay(500);
-        currentColor.style.backgroundColor = `${colors[item]["new"]}`;
-        await delay(600);
-        currentColor.style.backgroundColor = `${colors[item]["current"]}`;
-        await delay(600);
-    }
-    pathGenerator = false;
+  countCenter.innerText = count;
+  for (let item of randomColors) {
+    let currentColor = document.querySelector(`.${item}`);
+    await delay(500);
+    currentColor.style.backgroundColor = `${colors[item]["new"]}`;
+    await delay(600);
+    currentColor.style.backgroundColor = `${colors[item]["current"]}`;
+    await delay(600);
+  }
+  pathGenerator = false;
 };
 
 //Delay for blink effect to use on pathwayPlay()
@@ -71,11 +71,41 @@ async function delay(time) {
 
 //Function to get a random value from object
 const getRandomValue = (obj) => {
-    let arr = Object.keys(obj);
-    return arr[Math.floor(Math.random() * arr.length)];
+  let arr = Object.keys(obj);
+  return arr[Math.floor(Math.random() * arr.length)];
 };
 
 //Call functions on DOM
-startBtn.addEventListener("click", event => {
-    startGame();
+startBtn.addEventListener("click", (event) => {
+  startGame();
+});
+
+//When user click on the colors
+colorPart.forEach((element) => {
+  element.addEventListener("click", async (i) => {
+    //if user clicks the same color then next level
+    if (pathGenerator) {
+      return false;
+    } else if (
+      i.target.classList[0] == randomColors[clickCount]
+    ) {
+      //blick effect on click
+      i.target.style.backgroundColor = `${
+        colors[randomColors[clickCount]]["new"]
+      }`;
+      await delay(500);
+      i.target.style.backgroundColor = `${
+        colors[randomColors[clickCount]]["current"]
+      }`;
+      //User click
+      clickCount += 1;
+      //Next level if number of valid clicks == count
+      if (clickCount == count) {
+        clickCount = 0;
+        pathway();
+      }
+    } else {
+      lose();
+    }
+  });
 });
