3fbc8451c2c89c32a6e1c9475a274418df22d070
Julia Lavagnini Maia <julia.lavagninimaia@gmail.com>
2023-11-16 13:46:41 +0000

    Add Jest test file to test for sequence plays correct

--- assets/scripts/tests/main.test.js
+++ assets/scripts/tests/main.test.js
@@ -1,18 +1,58 @@
-/**
- * @jest-environment jsdom
- */
-const { startGame } = require("../main");
+// Import the necessary functions and objects from the main.js file
+const {
+  pathwayPlay,
+  delay,
+  getRandomValue,
+} = require("../main.js");
 
-beforeAll(() => {
-  let fs = require("fs");
-  let fileContents = fs.readFileSync("index.html", "utf-8");
-  document.open();
-  document.write(fileContents);
-  document.close();
+// Mocking the DOM interaction for testing
+beforeEach(() => {
+  document.body.innerHTML = `
+    <div id="count">0</div>
+    <div class="color-part" data-color="color1"></div>
+    <div id="score"></div>
+  `;
 });
 
-describe("game object contains correct keys", () => {
-  test("score key exists", () => {
-    expect("count" in startGame).toBe(true);
+// Mocking the delay and getRandomValue functions
+jest.mock("../main.js", () => ({
+  ...jest.requireActual("../main.js"),
+  delay: jest.fn(
+    (time) =>
+      new Promise((resolve) => setTimeout(resolve, time))
+  ),
+  getRandomValue: jest.fn(() => "color1"),
+}));
+
+describe("Color Blinking Tests", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
+
+  test("Test if the colors blink according to the sequence", async () => {
+    // Mock the initial state and call the pathwayPlay function
+    await pathwayPlay();
+
+    // Using Jest timers to advance time to simulate the delay
+    jest.advanceTimersByTime(1000);
+
+    // Ensuring that the count element's innerHTML had been updated
+    const countElement = document.getElementById("count");
+    expect(countElement.innerHTML).toBe("0");
+
+    // Ensuring that the color part's background color has changed
+    const colorPart = document.querySelector(".color-part");
+    expect(colorPart.style.backgroundColor).toBe("");
+  });
+
+});
+
+// Run all timers before each test
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+// Clean up after each test
+afterEach(() => {
+  jest.useRealTimers();
 });
